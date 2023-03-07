56607a2403e0876d8e163acc918241ee2931646d
justinjmueller <justin.mueller@colostate.edu>
2023-03-07 11:44:26 -0600

    Removed unused clustering function

--- include/utilities.h
+++ include/utilities.h
@@ -10,13 +10,8 @@
 #include "particle.h"
 
 std::vector<std::string> split(const std::string &s, char delim);
-std::pair<size_t, size_t> find_match(const IMatch& m,
-				     const std::vector<Interaction>& from_int,
-				     const std::vector<Interaction>& to_int);
-std::pair<size_t, size_t> find_match(const PMatch& m,
-				     const std::vector<Particle>& from_part,
-				     const std::vector<Particle>& to_part);
-std::vector<bool> find_best_cluster(const std::vector<Particle>& part, double thr);
+std::pair<size_t, size_t> find_match(const IMatch& m, const std::vector<Interaction>& from_int, const std::vector<Interaction>& to_int);
+std::pair<size_t, size_t> find_match(const PMatch& m, const std::vector<Particle>& from_part, const std::vector<Particle>& to_part);
 void parse_particle_string(std::string s, std::vector<uint16_t>& counts);
 bool match_strings(std::string s1, std::string s2);
 #endif
--- src/utilities.cpp
+++ src/utilities.cpp
@@ -19,9 +19,7 @@ std::vector<std::string> split(const std::string &s, char delim)
   return elems;
 }
 
-std::pair<size_t, size_t> find_match(const IMatch& m,
-				     const std::vector<Interaction>& from_int,
-				     const std::vector<Interaction>& to_int)
+std::pair<size_t, size_t> find_match(const IMatch& m, const std::vector<Interaction>& from_int, const std::vector<Interaction>& to_int)
 {
   std::pair<size_t, size_t> res(std::make_pair(-1,-1));
   for(size_t i(0); i < from_int.size(); ++i)
@@ -31,9 +29,7 @@ std::pair<size_t, size_t> find_match(const IMatch& m,
   return res;
 }
 
-std::pair<size_t, size_t> find_match(const PMatch& m,
-				     const std::vector<Particle>& from_part,
-				     const std::vector<Particle>& to_part)
+std::pair<size_t, size_t> find_match(const PMatch& m, const std::vector<Particle>& from_part, const std::vector<Particle>& to_part)
 {
   std::pair<size_t, size_t> res(std::make_pair(-1,-1));
   for(size_t i(0); i < from_part.size(); ++i)
@@ -41,40 +37,6 @@ std::pair<size_t, size_t> find_match(const PMatch& m,
   for(size_t i(0); i < to_part.size(); ++i)
     if(to_part.at(i).particle_index == m.to_index) res.second = i;
   return res;
-}
-
-std::vector<bool> find_best_cluster(const std::vector<Particle>& part, double thr)
-{
-  std::vector<std::pair<ROOT::Math::XYZPoint, std::vector<bool> > > distances;
-  std::vector<bool> primary;
-  for(const auto& p : part)
-  {
-    distances.push_back(std::make_pair(ROOT::Math::XYZPoint(p.vtx0x, p.vtx0y, p.vtx0z),
-				    std::vector<bool>()));
-    distances.push_back(std::make_pair(ROOT::Math::XYZPoint(p.vtx1x, p.vtx1y, p.vtx1z),
-				    std::vector<bool>()));
-    primary.push_back(p.primary);
-  }
-  for(auto& p0 : distances)
-  {
-    for(auto& p1 : distances)
-    {
-      p0.second.push_back(sqrt(pow(p0.first.X() - p1.first.X(), 2) +
-			       pow(p0.first.Y() - p1.first.Y(), 2) +
-			       pow(p0.first.Z() - p1.first.Z(), 2)) < thr);
-    }
-  }
-  uint16_t best_count(0);
-  size_t best_match(0);
-  for(size_t d(0); d < distances.size(); ++d)
-  {
-    auto c = std::count(distances.at(d).second.begin(),
-			distances.at(d).second.end(),
-			true);
-    if(c > best_count) best_match = d;
-    if(c == best_count && primary.at(d)) best_match = d;
-  }
-  return distances.at(best_match).second;
 }
 
 void parse_particle_string(std::string s, std::vector<uint16_t>& counts)
