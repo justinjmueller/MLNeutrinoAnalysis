ab76aaf0e79c8ad727d10863ff04e0e547aaebd9
justinjmueller <justin.mueller@colostate.edu>
2023-03-22 16:24:42 -0500

    Added contained particle variable to output

--- analysis.cpp
+++ analysis.cpp
@@ -83,6 +83,7 @@ int main()
     data.add_common_pvariable("primary", kPrimary, sPEverything);
     data.add_common_pvariable("range_reco_energy", kParticleRREnergy, sPEverything);
     data.add_common_pvariable("vertex_distance", kVertexDistance, sPEverything);
+    data.add_common_pvariable("contained", kPCount, sPContained);
 
     // Truth particle variables.
     data.add_truth_pvariable("neutrino", kPCount, sPNeutrino);
